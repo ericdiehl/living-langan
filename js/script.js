6fa33e67b8bf162f32621c67672e23e33f224016
Eric Diehl <eric.allan.diehl@gmail.com>
2017-09-23 10:28:55 -0400

    Total Refactor

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
 $(document).ready(function(){
-  // Initial Page Load
-  menu('home');
-  // Event Handler for Menu Bar
-  $('.menu').click(function(){
-    menu(this.id);
-  });
+  // Load Menu
+  $('#menu').load('menu.html');
 });
-
-// Dynamically Load Content
-function menu(link){
-  $('.w3-content').load('pages/' + link + '.html');
-}
